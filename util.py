0e7eacdf262f05e22132232de9c62108cc969b60
Jeff Wiroj <pasawee2@gmail.com>
2024-02-06 09:42:09 -0800

    Create utility functions

---
+++ util.py
@@ -0,0 +1,24 @@
+class AverageMeter:
+    def __init__(self, name):
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        return f"{self.name}: has an avg value of {self.avg}"
+
+
+def get_accuracy(preds, labels):
+    acc = (preds.argmax(dim=1) == labels).float().mean().item()
+    return acc
