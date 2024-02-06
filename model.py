f53a6ec987e9ed22262be71e25e6d64bbe326a34
Jeff Wiroj <pasawee2@gmail.com>
2024-02-06 09:33:45 -0800

    Added Densenet

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import timm
 import torch.nn.functional as F
-
+import torchvision
 from data import split_batch
 
 
@@ -25,10 +25,21 @@ def get_effnet(output_size):
     return model
 
 
+def get_convnet(output_size):
+    convnet = torchvision.models.DenseNet(
+        growth_rate=32,
+        block_config=(6, 6, 6, 6),
+        bn_size=2,
+        num_init_features=64,
+        num_classes=output_size,  # Output dimensionality
+    )
+    return convnet
+
+
 class ProtoNet(nn.Module):
     def __init__(self, proto_dim):
         super().__init__()
-        self.model = get_effnet(proto_dim)
+        self.model = get_convnet(proto_dim)  # get_effnet(proto_dim)
 
     @staticmethod
     def calculate_prototypes(features, targets):
